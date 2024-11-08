8a9a9212f3e5a2453762b83b6528781953a65abe
jompnavarro <navarrojomarni@gmail.com>
2024-11-08 20:41:55 +0800

    jompnavarro

---
+++ assets/scripts/main.js
@@ -0,0 +1,46 @@
+
+// function myfsum(x, y) {
+//     return x + y
+// }
+
+// console.log(myfsum(123,456))
+
+// function myfsub(x, y) {
+//     return x - y
+// }
+
+// console.log(myfsub(123,456))
+
+// function myfmult(x, y) {
+//     return x * y
+// }
+
+// console.log(myfmult(123,456))
+
+// function myfdiv(x, y) {
+//     return x / y
+// }
+
+// console.log(myfdiv(123,456))
+
+// function myfexp(x, y) {
+//     return x ** y
+// }
+
+// console.log(myfexp(123,456))
+
+// function myfmod(x, y) {
+//     return x % y
+// }
+
+// console.log(myfmod(123,456))
+
+
+for (let x=0; x <=100; x++){
+   remainder = x % 2
+   if (remainder != 0)
+   {
+    console.log(x)
+   }
+}
+   
